b94732a2c86e83a6041673556bdc27afe9a6e611
imeugenia <jevgenijazigisova@gmail.com>
2022-01-16 16:40:18 +0100

    Move init logic to the _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { createRenderer } from "fela";
+import { RendererProvider } from "react-fela";
+import Head from "next/head";
+import config from "../_data/config.json";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const fallbackRenderer = createRenderer();
+
+function MyApp({ Component, pageProps, renderer = fallbackRenderer }) {
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+
+    if (window.CMS) {
+      CMS.registerPreviewTemplate("landingPage", LandingPage);
+    }
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>{config.title}</title>
+        <meta name="description" content={config.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <RendererProvider renderer={renderer}>
+        <Component {...pageProps} />
+      </RendererProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,5 @@
-import { useEffect } from "react";
-import { createRenderer } from "fela";
-import { RendererProvider } from "react-fela";
-import Head from "next/head";
-import config from "../_data/config.json";
 import LandingPage from "../components/LandingPage";
 
-const renderer = createRenderer();
-
 export default function Home() {
-  useEffect(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", (user) => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-
-    if (window.CMS) {
-      CMS.registerPreviewTemplate("landingPage", LandingPage);
-    }
-  }, []);
-
-  return (
-    <div>
-      <Head>
-        <title>{config.title}</title>
-        <meta name="description" content={config.description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <RendererProvider renderer={renderer}>
-        <LandingPage />
-      </RendererProvider>
-    </div>
-  );
+  return <LandingPage />;
 }
